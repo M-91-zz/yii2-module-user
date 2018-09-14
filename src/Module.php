2cea5059cfe36d12265583ca8790660ae8615f7b
Marcelo de Andrade <andrade.asmarcelo@gmail.com>
2018-09-14 15:04:58 -0300

    Fix translation

--- src/Module.php
+++ src/Module.php
@@ -25,18 +25,19 @@ class Module extends \yii\base\Module
 
     public function registerTranslations()
     {
-        Yii::$app->i18n->translations['id5/rbac/*'] = [
+        Yii::$app->i18n->translations['id5.rbac'] = [
             'class' => 'yii\i18n\PhpMessageSource',
+            'forceTranslation' => true,
             'sourceLanguage' => 'pt-br',
             'basePath' => '@vendor/id5/yii2-rbac/src/messages',
             'fileMap' => [
-                'app' => 'app.php',
+                'id5.rbac' => 'app.php',
             ],
         ];
     }
 
     public static function t($category, $message, $params = [], $language = null)
     {
-        return Yii::t('app', $message, $params, $language);
+        return Yii::t('id5.rbac', $message, $params, $language);
     }
 }
