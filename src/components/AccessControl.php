296c1d9bf0c6ee926983b2158c8dc8f994ff5fc9
Marcelo de Andrade <andrade.asmarcelo@gmail.com>
2018-09-14 17:08:21 -0300

    Check superadmin [User] in access control component

--- src/components/AccessControl.php
+++ src/components/AccessControl.php
@@ -2,12 +2,16 @@
 
 namespace id5\rbac\components;
 
+use Yii;
 use yii\base\ActionFilter;
 
-class AccessControl extends ActionFilter
+class AccessControl extends \yii\filters\AccessControl
 {
-    public function beforeAction()
+    public function beforeAction($action)
     {
-        
+        if (Yii::$app->user->identity->isSuperadmin())
+            return true;
+
+        return parent::beforeAction($action);
     }
 }
