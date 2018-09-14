a8253ea44dc2fba29c0cc680f4c97a4e1f0ec709
Marcelo de Andrade <andrade.asmarcelo@gmail.com>
2018-09-14 17:02:37 -0300

    Add login

--- src/controllers/AuthController.php
+++ src/controllers/AuthController.php
@@ -2,12 +2,30 @@
 
 namespace id5\rbac\controllers;
 
+use Yii;
 use yii\web\Controller;
+use id5\rbac\models\LoginForm;
 
 class AuthController extends Controller
 {
     public function actionIndex()
     {
         return "Auth";
+    }
+
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        } else {
+            $model->password = '';
+            return $this->render('login', [
+                'model' => $model,
+            ]);
+        }
     }
 }
