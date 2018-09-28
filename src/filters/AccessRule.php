f67db798079df32922c9f5c513009f7b999c8e02
Marcelo de Andrade <andrade.asmarcelo@gmail.com>
2018-09-28 12:00:10 -0300

    Refactore AccessRule

--- src/filters/AccessRule.php
+++ src/filters/AccessRule.php
@@ -15,23 +15,19 @@ class AccessRule extends \yii\filters\AccessRule
             return true;
         
         foreach ($this->roles as $role) {
-            if ($role === 'admin') {
-                if (!$user->isGuest && $user->identity->isSuperadmin()) {
-                    return true;
-                }
-            }
-            elseif ($role === '?') {
-                if ($user->isGuest) {
-                    return true;
-                }
-            }
-            elseif ($role === '@') {
-                if (!$user->isGuest) {
-                    return true;
-                }
-            } 
-            elseif ($user->can($role)) {
-                return true;
+            switch ($role) {
+                case 'admin':
+                    if (!$user->isGuest && $user->identity->isSuperadmin()) 
+                        return true;
+                case '?':
+                    if ($user->isGuest) 
+                        return true;
+                case '@':
+                    if (!$user->isGuest) 
+                        return true;
+                default:
+                    if ($user->can($role)) 
+                        return true;
             }
         }
 
