e365ff9d9db5c24eeac878a3fcfbfa544c74457f
Marcelo de Andrade <andrade.asmarcelo@gmail.com>
2018-10-21 16:22:58 -0300

    Refactor [Role] model

--- src/models/Role.php
+++ src/models/Role.php
@@ -3,73 +3,9 @@
 namespace M91\UserModule\models;
 
 use Yii;
-use yii\base\Model;
 use yii\rbac\Item;
-use M91\UserModule\Module;
 
-class Role extends Model
+class Role extends AuthItem
 {
-    /**
-     * @var [string]
-     */
-    public $name;
-
-    /**
-     * @var [string]
-     */
-    public $description;
-
-    /**
-     * @var [string]
-     */
     public $type = Item::TYPE_ROLE;
-
-    public $authManager;
-
-    public function __construct()
-    {
-        $this->authManager = Yii::$app->authManager;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function rules()
-    {
-        return [
-            [['name', 'description'], 'required'],
-            [['name', 'description'], 'string'],
-            [['name', 'description'], 'trim'],
-            ['name', function ($attribute, $params, $validator) {
-                if ($this->authManager->getRole($this->name) !== null) {
-                    $this->addError($attribute, Module::t('app', 'Role name must be unique. [{name}] already exists', [
-                        'name' => $this->name,
-                    ]));
-                }
-            }],
-        ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function attributeLabels()
-    {
-        return [
-            'name' => Module::t('app', 'Name'),
-            'description' => Module::t('app', 'Description'),
-        ];
-    }
-
-    /**
-     * @return boolean
-     */
-    public function save(): bool
-    {
-        $role = $this->authManager->createRole($this->name);
-        $role->description = $this->description;
-
-        return $this->validate() && $this->authManager->add($role);
-    }
-
 }
