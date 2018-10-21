c535ab5a46a3efb9af9803869b7bdd5c274eee0d
Marcelo de Andrade <andrade.asmarcelo@gmail.com>
2018-10-21 14:50:30 -0300

    Fix property call

--- src/views/role/update.php
+++ src/views/role/update.php
@@ -6,11 +6,11 @@ use M91\UserModule\Module;
 /* @var $this yii\web\View */
 /* @var $model M91\UserModule\models\Role */
 
-$this->title = Module::t('app', 'Update Role: ' . $model->id, [
-    'nameAttribute' => '' . $model->id,
+$this->title = Module::t('app', 'Update Role: ' . $model->name, [
+    'nameAttribute' => '' . $model->name,
 ]);
 $this->params['breadcrumbs'][] = ['label' => Module::t('app', 'Roles'), 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'name' => $model->id]];
+$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'name' => $model->name]];
 $this->params['breadcrumbs'][] = Module::t('app', 'Update');
 ?>
 
