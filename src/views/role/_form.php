20d94c607630517960d9e33f74e90e241e35593b
Marcelo de Andrade <andrade.asmarcelo@gmail.com>
2018-10-22 18:02:50 -0300

    Add select2 to role childrens

--- src/views/role/_form.php
+++ src/views/role/_form.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use M91\UserModule\Module;
+use kartik\select2\Select2;
 
 /* @var $this yii\web\View */
 /* @var $model M91\UserModule\models\Role */
@@ -16,6 +17,19 @@ use M91\UserModule\Module;
     <?= $form->field($model, 'name')->textInput(['disabled' => !$model->isNewRecord()]) ?>
 
     <?= $form->field($model, 'description')->textInput() ?>
+
+    <?= $form->field($model, 'items')->widget(Select2::classname(), [
+        'data' => $model->getItems(),
+        'options' => [
+            'placeholder' => 'Select a color ...',
+            'multiple' => true
+        ],
+        'pluginOptions' => [
+            'tags' => true,
+            'tokenSeparators' => [',', ' '],
+        ],
+    ]);
+    ?>
 
     <div class="form-group">
         <?= Html::submitButton(Module::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
